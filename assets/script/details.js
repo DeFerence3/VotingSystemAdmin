da4e5e25d736298267a19d20d8b0791bfa4820b7
danielpaul-123 <danielpaulperinchery07012005@gmail.com>
2023-05-27 11:54:11 +0530

    election result, index icon fixed

--- assets/script/details.js
+++ assets/script/details.js
@@ -3,6 +3,10 @@ window.onload = detailFetcher;
 
 const bk = document.getElementById("bkbutton");
 var table = document.getElementById("detailstable");
+var table2 = document.createElement("table");
+table2.setAttribute("id", "ElectionStats");
+table2.setAttribute("class", "tablee");
+table2.setAttribute("style","margin-top:20px;");
 
 function detailFetcher() {
   const param = new URLSearchParams(location.search);
@@ -32,13 +36,9 @@ function detailFetcher() {
             var newrow = document.createElement("tr");
             var newhead = document.createElement("th");
             var newtd = document.createElement("td");
-
             const cntid = "Contestant " + id;
-
             newhead.textContent = cntid;
             newtd.textContent = doc.data()[ctid];
-
-
             newrow.appendChild(newhead);
             newrow.appendChild(newtd);
             table.appendChild(newrow);
@@ -63,7 +63,7 @@ function detailFetcher() {
 
               newrow.appendChild(newhead);
               newrow.appendChild(newtd);
-              table.appendChild(newrow);
+              table2.appendChild(newrow);
             })
             .catch(error => {
             console.error("Error getting collection documents: ", error);
@@ -83,10 +83,52 @@ function detailFetcher() {
 
               newrow.appendChild(newhead);
               newrow.appendChild(newtd);
-              table.appendChild(newrow);
+              table2.appendChild(newrow);
             })
             .catch(error => {
             console.error("Error getting collection documents: ", error);
+          });
+
+          const elcstat = db.collection("Election_Stats");
+          elcstat.doc(elcId).get().then((doc) => {
+            
+            const elcdt = db.collection("Election_Data");
+            elcdt.doc(elcId).get().then((doc2) => 
+            {
+              for (var id = 1; id<=100; id++)
+              {
+              const ctid = "Contestant" + id;
+              if(doc.data().hasOwnProperty(ctid))
+              {
+
+                var newrow = document.createElement("tr");
+                var newhead = document.createElement("th");
+                var newtd = document.createElement("td");
+                newhead.textContent = doc2.data()[ctid];
+                newtd.textContent = doc.data()[ctid];
+                newrow.appendChild(newhead);
+                newrow.appendChild(newtd);
+                table2.appendChild(newrow);
+              }
+              else
+              {
+                id=101;
+              }
+            }
+            var newrow = document.createElement("tr");
+            var newhead = document.createElement("th");
+            var newtd = document.createElement("td");
+            newhead.textContent = "NOTA";
+            newtd.textContent = doc.data()["NOTA"];
+            newrow.appendChild(newhead);
+            newrow.appendChild(newtd);
+            table2.appendChild(newrow);
+            document.body.appendChild(table2);
+            }).catch(error => {
+              console.error("Error!",error);
+            });  
+          }).catch(error => {
+            console.error("Error!",error);
           });
 
       } else {
